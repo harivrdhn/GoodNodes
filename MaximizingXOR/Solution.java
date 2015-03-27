7dd06cbbb8de460260dc9cff3f30cc0042e9f252
Harivardhan Pyaram <p.hari.vrdhn@gmail.com>
2015-03-27 10:08:04 -0700

    Create Solution.java

---
+++ MaximizingXOR/Solution.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int maxXor(int l, int r) {
+        int max = 0;
+        for(int i = l; i < r; i++){
+            for(int j = i+1; j <=r; j++){
+                max = Math.max(max,i ^ j);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int res;
+        int _l;
+        _l = Integer.parseInt(in.nextLine());
+        
+        int _r;
+        _r = Integer.parseInt(in.nextLine());
+        
+        res = maxXor(_l, _r);
+        System.out.println(res);
+        
+    }
+}
